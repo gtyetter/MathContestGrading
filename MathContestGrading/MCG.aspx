7a3882aa7680cd20c274e523912fb59f82bca7cf
gtyetter <gtyetter@gmail.com>
2016-10-19 15:56:37 -0500

    Began working on the base project for the webpage

---
+++ MathContestGrading/MCG.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MCG.aspx.cs" Inherits="MathContestGrading.MCG" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Math Contest Grading</title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+    <div class="Input">
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="SaveButton" runat="server" Text="Save" OnClick="SaveButton_Click"/>
+    </div>
+    <div class="validate"></div>
+    <div class="grade"></div>
+    <div class="output"></div>
+    </form>
+</body>
+</html>
---
+++ MathContestGrading/MCG.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MathContestGrading
+{
+    public partial class MCG : System.Web.UI.Page
+    {
+        protected class contestant
+        {
+
+        }
+
+        protected List<contestant> contestants = new List<contestant>();
+
+        public static void parse()
+        {
+
+        }
+
+        public static void grade()
+        {
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
