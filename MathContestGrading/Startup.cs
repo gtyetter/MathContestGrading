1ed09e83abb7e39fe56c5a4d9c0d4d340dada15a
gtyetter <gtyetter@gmail.com>
2016-10-19 15:05:49 -0500

    Add project files.

---
+++ MathContestGrading/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MathContestGrading.Startup))]
+namespace MathContestGrading
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
