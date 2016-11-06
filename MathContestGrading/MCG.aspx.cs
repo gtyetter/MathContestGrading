8e6ca48892a26dfda8afda6358f050b0c9849e67
Grant <gtyetter@gmail.com>
2016-11-06 16:47:25 -0600

    not done yet

--- MathContestGrading/MCG.aspx.cs
+++ MathContestGrading/MCG.aspx.cs
@@ -493,7 +493,16 @@ namespace MathContestGrading
 
         protected void DownloadFilesBtn_Click(object sender, EventArgs e)
         {
-
+            String FileName = "FileName.txt";
+            String FilePath = "C:/...."; //Replace this
+            System.Web.HttpResponse response = System.Web.HttpContext.Current.Response;
+            response.ClearContent();
+            response.Clear();
+            response.ContentType = "text/plain";
+            response.AddHeader("Content-Disposition", "attachment; filename=" + FileName + ";");
+            response.TransmitFile(FilePath);
+            response.Flush();
+            response.End();
         }
     }
 }
